13d3d7c47cf9ef12093397dbbce8aa2d462955e3
yunekorea <kooopooo@naver.com>
2019-04-10 08:33:53 -0700

    second commit

--- hello.c
+++ hello.c
@@ -2,5 +2,6 @@
 
 int main() {
 	printf("hello, Open Source Programming\n");
+	printf("putting additional comment.\n");
 	return 0;
 }
---
+++ second.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("second commitment of github.\n");
+	return 0;
+}
